01d8f5888143dbcc50477a0a01fcbabc5012a7c3
Gaetano <64538010+skrtdev@users.noreply.github.com>
2020-09-09 23:39:18 +0200

    Update 1.x.php

--- src/bc/1.x.php
+++ src/bc/1.x.php
@@ -4,7 +4,7 @@ namespace Telegram;
 
 class Bot extends \NovaGram\Bot{
     public function __construct(...$args){
-        #trigger_error('Using deprecated '.self::class.', use \\'.parent::class.' instead');
+        trigger_error('Using deprecated '.self::class.', use \\'.parent::class.' instead');
         parent::__construct(...$args);
         $this->json_payload = false;
     }
