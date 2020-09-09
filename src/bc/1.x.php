29e377efc86434d0b9a04a390031f5442fe7a846
Gaetano <64538010+skrtdev@users.noreply.github.com>
2020-09-09 23:39:52 +0200

    Update 1.x.php

--- src/bc/1.x.php
+++ src/bc/1.x.php
@@ -6,7 +6,6 @@ class Bot extends \NovaGram\Bot{
     public function __construct(...$args){
         trigger_error('Using deprecated '.self::class.', use \\'.parent::class.' instead');
         parent::__construct(...$args);
-        $this->json_payload = false;
     }
 }
 
