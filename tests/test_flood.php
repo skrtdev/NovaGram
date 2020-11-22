364f4291942d8a2b7e21bf0893e0f51fb7157b58
skrtdev <64538010+skrtdev@users.noreply.github.com>
2020-11-22 18:30:16 +0100

    Catch TooManyRequestsException

--- tests/test_flood.php
+++ tests/test_flood.php
@@ -3,7 +3,7 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use skrtdev\NovaGram\Bot;
-use skrtdev\Telegram\Exception as TelegramException;
+use skrtdev\Telegram\TooManyRequestsException;
 
 use skrtdev\async\Pool;
 
@@ -15,7 +15,17 @@ $chat_id = readline("Insert your chat_id: ");
 
 for ($i=0; $i < 300; $i++) {
     $pool->parallel(function () use ($Bot, $chat_id) {
-        $Bot->sendMessage($chat_id, "a");
+        while (true) {
+            try{
+                $Bot->sendMessage($chat_id, "a");
+                break;
+            }
+            catch(TooManyRequestsException $e){
+                $seconds = $e->response['parameters']['retry_after'];
+                echo "Sleeping for $seconds seconds", PHP_EOL;
+                sleep($seconds);
+            }
+        }
     });
 }
 
