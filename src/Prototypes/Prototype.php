2cf033b556fce8bc4a19f7602c8df1a070ca55a1
skrtdev <64538010+skrtdev@users.noreply.github.com>
2020-12-31 13:31:40 +0100

    v2 deprecations

--- src/Prototypes/Prototype.php
+++ src/Prototypes/Prototype.php
@@ -2,33 +2,36 @@
 
 namespace skrtdev\Prototypes;
 
-use ReflectionFunction;
+use Closure, ReflectionFunction, Error;
 
 class Prototype{
 
     public static array $methods = [];
 
-    public static function addMethod(string $class_name, string $name, \Closure $fun): bool{
+    public static function addMethod(string $class_name, string $name, Closure $fun): void
+    {
         self::$methods[$class_name] ??= [];
         self::$methods[$class_name][$name] = $fun;
-        return true;
     }
 
-    public static function call(object $obj, string $name, array $args){
+    public static function call(object $obj, string $name, array $args)
+    {
         $class_name = get_class($obj);
         self::$methods[$class_name] ??= [];
         if(isset(self::$methods[$class_name][$name])){
             $closure = self::$methods[$class_name][$name];
-
+            // DEPRECATE SELF IN v2.0
             $reflection = new ReflectionFunction($closure);
             $parameters = $reflection->getParameters();
             $has_self = isset($parameters[0]) && $parameters[0]->getName() === "self";
 
             $closure_args = [ ...($has_self ? [$obj] : []), ...$args ];
             return $closure->call($obj, ...$closure_args);
+            // v2:
+            // return $closure->call($obj, ...$args);
         }
         else{
-            throw new \Error("Call to undefined method $class_name::$name()");
+            throw new Error("Call to undefined method $class_name::$name()");
         }
     }
 
