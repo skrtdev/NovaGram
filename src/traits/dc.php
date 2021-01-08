7e03a685e6135aad59aed61fb72418211c8e3857
skrtdev <64538010+skrtdev@users.noreply.github.com>
2021-01-08 18:48:07 +0100

    cache user/chat dc

--- src/traits/dc.php
+++ src/traits/dc.php
@@ -4,7 +4,7 @@ namespace skrtdev\NovaGram;
 trait dc{
     public function getDC() {
         if(!isset($this->username)) throw new Exception("{$this->_} passed to getDC has not an username");
-        return Bot::getUsernameDC($this->username);
+        return $this->dc_id ??= Bot::getUsernameDC($this->username);
     }
 }
 
